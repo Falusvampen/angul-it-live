29dba2873ab730d7e354ca445a26fa41eabd20f8
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-27 16:10:02 +0300

    finished recaptcha testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RECAPTCHA_SETTINGS, RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings } from 'ng-recaptcha';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
@@ -16,9 +18,19 @@ import { ResultComponentComponent } from './result-component/result-component.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+    provide: RECAPTCHA_SETTINGS,
+    useValue: {
+      siteKey: environment.recaptcha.siteKey,
+    } as RecaptchaSettings,
+  },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
