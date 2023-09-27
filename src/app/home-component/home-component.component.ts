dfb90f6d11e8040942621c20e662c06f0c4a86c1
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-27 23:31:36 +0300

    feat: Update home component UI and functionality
    - Update welcome message in the HTML file
    - Add completed levels buttons for each completed stage
    - Refactor stage sections in the HTML file
    - Implement functionality for advancing to the next stage upon captcha or question success
    - Store and retrieve current stage using sessionStorage
    - Add function to navigate to a specific stage by clicking on a completed level button

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -6,30 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-component.component.scss']
 })
 export class HomeComponentComponent implements OnInit {
-
+  completedStages: number[] = [1];
   currentStage = 1; 
 
   constructor() { }
 
   ngOnInit(): void {
-    
     const savedStage = sessionStorage.getItem('currentStage');
-
-    
     if (savedStage !== null) {
-      this.currentStage = Number(savedStage);  
+      this.currentStage = Number(savedStage);
     }
   }
 
   onCaptchaSuccess(): void {
-
-    this.currentStage++;
-
-    
-    sessionStorage.setItem('currentStage', this.currentStage.toString());
-
-    
-    console.log('Current stage stored:', this.currentStage);
+    this.advanceStage();
   }
 
+  onMathQuestionPassed(): void {
+    this.advanceStage();
+  }
+
+  onRandomQuestionPassed(): void {
+    this.advanceStage();
+  }
+
+  advanceStage(): void {
+    this.currentStage++;
+    // Check if the stage is already completed
+    if (!this.completedStages.includes(this.currentStage)) {
+      this.completedStages.push(this.currentStage);
+    }
+    sessionStorage.setItem('currentStage', this.currentStage.toString());
+    console.log(`Current stage stored: ${this.currentStage}`);
+  }
+
+  navigateToStage(stage: number): void {
+    this.currentStage = stage;
+    sessionStorage.setItem('currentStage', stage.toString());
+  }
 }
