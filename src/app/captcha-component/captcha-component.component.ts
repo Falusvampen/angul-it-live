17cd33e7ae39ccb7d2bb29fbbf22aa863391f453
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-09-27 17:36:23 +0300

    feat: Add captcha-component UI and functionality
    - Added HTML code for the captcha-component, including form, reCAPTCHA element, and submit button.
    - Updated the styling of the captcha-component to display an error border when invalid.
    - Modified the captcha-component TypeScript file to emit a success event when the form is valid and reset the form.
    - Updated the home-component HTML to include the captcha-component in different stages.
    - Updated the home-component TypeScript file to handle the success event and advance to the next stage.

--- src/app/captcha-component/captcha-component.component.ts
+++ src/app/captcha-component/captcha-component.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component , Output, EventEmitter} from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-captcha-component',
   templateUrl: './captcha-component.component.html',
   styleUrls: ['./captcha-component.component.scss']
 })
+
+
 export class CaptchaComponentComponent {
 
+  @Output() success = new EventEmitter<void>();
+  token: string | undefined;
+
+  constructor() {
+    this.token = undefined;
+  }
+
+  public send(form: NgForm): void {
+    if (form.invalid) {
+      for (const control of Object.keys(form.controls)) {
+        form.controls[control].markAsTouched();
+      }
+      return;
+    }
+    this.success.emit();
+    console.debug(`Token [${this.token}] generated`);
+    form.reset();
+  }
 }
--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.scss']
 })
-export class HomeComponentComponent {
+export class HomeComponentComponent implements OnInit{
+
+  currentStage = 1; // Initialize to stage 1
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCaptchaSuccess(): void {
+    this.currentStage++; // Increment to the next stage
+  }
 
 }
